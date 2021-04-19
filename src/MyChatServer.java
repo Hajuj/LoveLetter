dfa0012f294b3a91864976a4c9b68051a392abee
Altug Utas <utas@cip.ifi.lmu.de>
2021-04-19 17:12:57 +0200

    Basis of Server and UserThread

---
+++ src/MyChatServer.java
@@ -0,0 +1,60 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+public class MyChatServer {
+    private static int port = 1337;
+    private Set<UserThread> userThreads = new HashSet<>();
+    private Set<String> usernames = new HashSet<>();
+
+    public MyChatServer(int port) {
+        MyChatServer.port = port;
+    }    
+
+    public void execute() {
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Chat server is online on " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New user connected");
+
+                UserThread newUserThread = new UserThread(socket, this);
+                userThreads.add(newUserThread);
+                newUserThread.start();
+            }
+        } catch (Exception e) {
+            System.out.println("Error in the server: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+ 
+        MyChatServer server = new MyChatServer(port);
+        server.execute();
+    }
+
+    public void addUsername(String username) {
+        usernames.add(username);
+    }
+
+    public void broadcast(String message, UserThread excludedUser) {
+        for (UserThread user : userThreads) {
+            if (user != excludedUser) {
+                user.sendMessage(message);
+            }
+        }
+    }
+
+    public void removeUser(String username, UserThread user) {
+        boolean removed = usernames.remove(username);
+        if (removed) {
+            userThreads.remove(user);
+            System.out.println("The user " + username + " quitted");
+        }
+    }
+}
