87ec44a95cf56b99f1a136b67312aac9ac6de3db
Viktoryia Patapovich <patapovich@cip.ifi.lmu.de>
2021-04-19 22:29:10 +0200

    first try 0.1

---
+++ Chat/src/Chat1904/MyChatServer.java
@@ -0,0 +1,71 @@
+package Chat1904;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+import java.util.Set;
+
+
+
+
+public class MyChatServer {
+    private static int port = 47329;
+    private Set<UserThread> userThreads = new HashSet<>();
+    private Set<String> usernames = new HashSet<>();
+
+    public MyChatServer(int port) {
+        MyChatServer.port = port;
+    }
+
+    public Set<String> getUsernames() {
+        return usernames;
+    }
+
+    public void execute() {
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Chat server ist nun online!");
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New user connected");
+
+                UserThread newUserThread = new UserThread(socket, this);
+                userThreads.add(newUserThread);
+                newUserThread.start();
+            }
+        } catch (Exception e) {
+            System.out.println("Error in the server: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyChatServer server = new MyChatServer(port);
+        server.execute();
+    }
+
+    public void addUsername(String username) {
+        usernames.add(username);
+    }
+
+    public void broadcast(String message, UserThread excludedUser) {
+
+        for (UserThread user : userThreads) {
+            if (user != excludedUser && !message.isBlank()) {
+                user.sendMessage(message);
+            }
+        }
+    }
+
+    public void directMessage(String message, UserThread user) {
+        user.sendMessage(message);
+    }
+
+    public void removeUser(String username, UserThread user) {
+        boolean removed = usernames.remove(username);
+        if (removed) {
+            userThreads.remove(user);
+            System.out.println("The user " + username + " left the room");
+        }
+    }
+}
