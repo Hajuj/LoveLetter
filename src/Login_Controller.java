2a38aecf1ba4ae6a6f24e63f15d8459cc4c160cd
Chiara Welz <welzc@cip.ifi.lmu.de>
2021-04-20 13:06:05 +0200

    FE Login: Main, Controller & .fxml connected; Login open

---
+++ src/Login_Controller.java
@@ -0,0 +1,71 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Login_Controller {
+
+    public TextField username;
+
+    public Button btn_signIn;
+
+    public Label serverReturn;
+
+    //public void initialize() {inputTextfield.clear();}
+    @FXML
+    private void handleSubmitButtonAction(ActionEvent event){
+        btn_signIn.setDisable(true);
+        String text = username.getText();
+
+        /*if (text.equals("")) {
+            btn_signIn.setText("Well... If you want me to say nothing, then that's the way it is.");
+            username.setDisable(true);
+            return;
+        } else if (!Model.getInstance().waseverSaid(text)) {
+            btn_signIn.setText("Ok.. here you go: " + text);
+        } else {
+            btn_signIn.setText("Nah! I already said that!");
+        }*/
+        username.clear();
+
+        btn_signIn.setDisable(false);
+    }
+    /*
+    //private final MyChatServer chatServer;
+
+    public Login_Controller(MyChatServer chatServer){
+        this.chatServer = chatServer;
+    }
+
+    @FXML private javafx.scene.control.TextField username;
+
+    @FXML protected void btnSignIn() {
+        //Schleife: Rückmeldung von Server
+
+        //Beim Senden soll der Nickname im MyChatServer überprüft und gespeichert werden
+        chatServer.addUsername(username.getText());
+
+        //Fehlerrückmeldung
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+    }*/
+
+}
+
+
+ 
---
+++ src/Main.java
@@ -0,0 +1,80 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main extends Application {
+
+    //Button button;
+
+    public ArrayList<Thread> threads;
+    //private Object MyChatServer;
+
+
+    //wenn Programm Parameter enthält;
+    //führt erst init() dann start(...) aus;
+    //wartet dann auf das Programmende und führt stop () aus;
+    //sollte nicht überschrieben werden;
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public static Scene scene;
+
+    @Override
+    public void stop() throws Exception {
+        // Auto-generated method stub
+        super.stop();
+        for (Thread thread: threads){
+            thread.interrupt();
+        }
+    }
+
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
+        scene = new Scene(fxmlLoader.load());
+
+        primaryStage.setTitle("LoveLetters");
+        primaryStage.setScene(scene);
+        scene.getStylesheets().add("Login.css");
+        primaryStage.show();
+
+
+
+        /* try {
+
+            primaryStage.setTitle("LoveLetters");
+
+            threads = new ArrayList<Thread>();
+
+            //Load Model: MyChatServer.java
+            MyChatServer model = new MyChatServer();
+
+            //Load FXML-Datei: Login.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
+            Parent root = loader.load();
+
+            //Load Controller: Login_Controller.java & deklarieren
+            Login_Controller controller;
+            controller = new Login_Controller(model);
+            loader.setController(controller);
+
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            scene.getStylesheets().add(getClass().getResource("Login.css").toExternalForm());
+
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("LoveLetters");
+            //primaryStage.setX(10);
+            primaryStage.show();
+
+        */} /*catch(Exception e) {
+            e.printStackTrace();
+        }*/
+    }
+
