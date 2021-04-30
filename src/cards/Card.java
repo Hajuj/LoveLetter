c8cefe76bc25a036d81265369fafd62e39913fa5
Viktoryia Patapovich <patapovich@cip.ifi.lmu.de>
2021-04-30 13:44:19 +0200

    refractoring and some changes

---
+++ src/cards/Card.java
@@ -0,0 +1,99 @@
+package cards;
+
+import game.Player;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * The type Card.
+ */
+public class Card {
+    /**
+     * The Card number.
+     */
+    public int cardNumber;
+    /**
+     * The Number of cards in deck.
+     */
+    public int numberOfCardsInDeck;
+    /**
+     * The Card text.
+     */
+    public String cardText;
+    /**
+     * The Card name.
+     */
+    public String cardName;
+
+    /**
+     * Instantiates a new Card.
+     *
+     * @param cardNumber          the card number
+     * @param numberOfCardsInDeck the number of cards in deck
+     * @param cardText            the card text
+     * @param cardName            the card name
+     */
+    public Card(int cardNumber, int numberOfCardsInDeck, String cardText, String cardName) {
+        this.cardNumber = cardNumber;
+        this.numberOfCardsInDeck = numberOfCardsInDeck;
+        this.cardText = cardText;
+        this.cardName = cardName;
+    }
+
+    /**
+     * Choose a player int.
+     *
+     * @param numberOfPlayers the number of players
+     * @param listOfPlayers   the list of players
+     * @return the int
+     */
+    public int chooseAPlayer(int numberOfPlayers, ArrayList<Player> listOfPlayers) {
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while (loop) {
+            System.out.println("Please select a player:");
+            for (int i = 1; i < numberOfPlayers + 1; i++) {
+                System.out.println(i + ". Player " + i);
+            }
+            int selection = scanner.nextInt();
+            selection--;
+            loop = listOfPlayers.get(selection).isHandmaidOnline;
+            if (!loop) {
+                int printSelection = selection + 1;
+                System.out.println("You've Chosen Player " + printSelection);
+                return selection;
+            } else {
+                System.out.println("You've chosen a player protected by the Handmaid. Please select another player.");
+            }
+
+        }
+        return -1;
+    }
+
+    /**
+     * Perform card instruction.
+     *
+     * @param currentPlayer              the current player
+     * @param currentRoundsListOfPlayers the current rounds list of players
+     * @param thisRoundsDeck             the this rounds deck
+     */
+    public void performCardInstruction(Player currentPlayer, ArrayList<Player> currentRoundsListOfPlayers, Deck thisRoundsDeck) {
+        System.out.println("Sth Went Wrong, this operation should call to specific card's interaction.");
+    }
+
+    /**
+     * Discard card after using.
+     *
+     * @param currentPlayer       the current player
+     * @param idOfTheCardToRemove the id of the card to remove
+     */
+    public void discardCardAfterUsing(Player currentPlayer, int idOfTheCardToRemove) {
+        for (int i = 0; i < currentPlayer.playerCardList.size(); i++) {
+            if (currentPlayer.playerCardList.get(i).cardNumber == idOfTheCardToRemove) {
+                currentPlayer.playerCardList.remove(i);
+                break;
+            }
+        }
+    }
+}
---
+++ src/chat/Client.java
@@ -0,0 +1,235 @@
+package chat;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * The type chat.Client.
+ */
+/*chat.Client Class für Socket Verbindungen der Threads*/
+public class Client {
+    /**
+     * The chat.Connection.
+     */
+    protected Connection connection = new Connection(new Socket(getServerAddress(), getServerPort()));
+    private volatile boolean clientConnected;
+
+    /**
+     * Instantiates a new chat.Client.
+     *
+     * @throws IOException the io exception
+     */
+    public Client() throws IOException {
+    }
+
+    /**
+     * Send text message.
+     *
+     * @param text the text
+     */
+    /*Nachricht senden an alle*/
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Error beim Senden");
+            clientConnected = false;
+        }
+    }
+
+    /**
+     * Run.
+     */
+    /*Run Methode für die chat.Client Verbindung per Sockets*/
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        // thread ist daemon
+        socketThread.setDaemon(true);
+        socketThread.run();
+
+        try {
+            synchronized (this) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            ConsoleHelper.writeMessage("Fehler ");
+            return;
+        }
+
+        if (clientConnected)
+            ConsoleHelper.writeMessage("You are connected");
+        else
+            ConsoleHelper.writeMessage("Fehler");
+
+        while (clientConnected) {
+            String text = ConsoleHelper.readString();
+
+            if (shouldSendTextFromConsole())
+                sendTextMessage(text);
+        }
+    }
+
+    /**
+     * Gets server address.
+     *
+     * @return the server address
+     */
+    /*Getter Methoden für IP, Port und Name - Vorerst aber fest definiert bei "127.0.0.1" und 500*/
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("chat.Server IP:");
+        return ConsoleHelper.readString();
+    }
+
+    /**
+     * Gets server port.
+     *
+     * @return the server port
+     */
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("chat.Server Port:");
+        return ConsoleHelper.readInt();
+    }
+
+    /**
+     * Gets user name.
+     *
+     * @return the user name
+     */
+    protected String getUserName() {
+        ConsoleHelper.writeMessage("Your name:");
+        return ConsoleHelper.readString();
+    }
+
+    /**
+     * Gets socket thread.
+     *
+     * @return the socket thread
+     */
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    /**
+     * Should send text from console boolean.
+     *
+     * @return the boolean
+     */
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    /**
+     * The type Socket thread.
+     */
+    /*Run Methode für Handshake und Mainloop*/
+    public class SocketThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                clientHandshake();
+                clientMainLoop();
+
+            } catch (IOException | ClassNotFoundException e) {
+                notifyConnectionStatusChanged(false);
+            }
+        }
+
+        /**
+         * chat.Client handshake.
+         *
+         * @throws IOException            the io exception
+         * @throws ClassNotFoundException the class not found exception
+         */
+        /*client Handshake um die Nachrichten zu synchronisieren*/
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            // TODO maybe make it smarter? eliminate busy waiting -> synchronize block rather than while.
+            String name = null;
+            while (true) {
+                Message message = connection.receive();
+
+                if (message.getType() == MessageType.NAME_REQUEST) { // ask the name
+                        name = getUserName();
+                        connection.send(new Message(MessageType.USER_NAME, name));
+                        this.notifyConnectionStatusChanged(false);
+                    /*TBD neue Funktion um direkte Nachrichten zu senden*/
+//                    name = getUserName();
+//                    connection.send(new chat.Message(chat.MessageType.USER_NAME, name));
+//                    this.notifyConnectionStatusChanged(false);
+
+                } else if (message.getType() == MessageType.NAME_ACCEPTED) { // server accepted the name
+                    this.notifyConnectionStatusChanged(true);
+                    return;
+
+                } else {
+                    throw new IOException("Unexpected chat.MessageType");
+                }
+            }
+        }
+
+        /**
+         * chat.Client main loop.
+         *
+         * @throws IOException            the io exception
+         * @throws ClassNotFoundException the class not found exception
+         */
+        /*Verwaltung der Art von Informationen die über den chat.Server laufen*/
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.TEXT) {
+                    processIncomingMessage(message.getData());
+                } else if (MessageType.USER_ADDED == message.getType()) {
+                    informAboutAddingNewUser(message.getData());
+                } else if (MessageType.USER_REMOVED == message.getType()) {
+                    informAboutDeletingNewUser(message.getData());
+                } else {
+                    throw new IOException("Unexpected chat.MessageType");
+                }
+            }
+        }
+
+        /**
+         * Process incoming message.
+         *
+         * @param message the message
+         */
+        /* Weitergabe der Nachricht*/
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+        }
+
+        /**
+         * Inform about adding new user.
+         *
+         * @param userName the user name
+         */
+        /*Begrüßung bei Anmeldung */
+        protected void informAboutAddingNewUser(String userName) {
+            ConsoleHelper.writeMessage("Welcome " + userName + "!");
+        }
+
+        /**
+         * Inform about deleting new user.
+         *
+         * @param userName the user name
+         */
+        /*Benachrichtigung über Abmeldung */
+        protected void informAboutDeletingNewUser(String userName) {
+            ConsoleHelper.writeMessage("Username  '" + userName + "' left.");
+        }
+
+        /**
+         * Notify connection status changed.
+         *
+         * @param clientConnected the client connected
+         */
+        /*Benachrichtigung über Anmeldung */
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this) {
+                Client.this.notify();
+            }
+        }
+    }
+}
