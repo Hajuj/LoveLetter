1d533a2a901a36656ead2383275b0712e3b92b0c
Jonas Gottal <gottal@cip.ifi.lmu.de>
2021-05-09 18:00:40 +0200

    Final Todos

--- src/chat/ClientGuiController.java
+++ src/chat/ClientGuiController.java
@@ -69,7 +69,6 @@ public class ClientGuiController extends Client {
      * Refresh messages.
      */
     public synchronized void refreshMessages() {
-        // TODO  Prio 1: replace with bindings
         Platform.runLater(() -> messages.appendText(getModel().getNewMessage() + "\n"));
     }
 
